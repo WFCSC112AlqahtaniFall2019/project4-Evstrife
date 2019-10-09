b8378570c119043a2234da3598b7124158b50482
evstrife <wus217@wfu.edu>
2019-10-09 19:52:59 -0400

    FINAL VERSION

--- TTTGame.cpp
+++ TTTGame.cpp
@@ -8,44 +8,111 @@
 using namespace std;
 
 int main() {
-    int player;
+    int row1, col1;
     int numPlayers;
-    string playagain = "Yes";
+    char playagain;
 
 
-        TicTacToe game;
-        //initialize the game board
-        game.initializeBoard();
-        //print the board
-        game.printBoard();
-        //user enter the number of players
-        cout << "Please Enter you are 1 player or 2 players?" << endl;
-        cin >> numPlayers;
+    TicTacToe game;
+    //initialize the game board
+    game.initializeBoard();
+    //print the board
+    game.printBoard();
+    //user enter the number of players
+    cout << "Please Enter you are 1 player or 2 players?" << endl;
+    cin >> numPlayers;
 
 
-        //check if the input is 1 or 2
-        if (numPlayers != 1 && numPlayers != 2) {
-            cout << "Please try again and enter either 1 or 2 players." << endl;
-            return 0;
-        }
-        else if (numPlayers == 1){
-            cout<< "You are playing against computer"<<endl;
-            srand(time(0));
-            player = rand()% 2;
-            if(player == 1){
-                game.changePlayer();
-                cout << "You are player " << game.getCurrentPlayerNumber() << " playing as the o" << endl;
+    //check if the input is 1 or 2
+    if (numPlayers != 1 && numPlayers != 2) {
+        cout << "Please try again and enter either 1 or 2 players." << endl;
+        return 0;
+    }
+        //one player start
+    else if (numPlayers == 1) {
+        while (playagain != 'n') {
+            srand(time(nullptr));
+            int first = rand() % 2 + 1;
+            cout << "You will be playing against the computer." << endl;
+            game.initializeBoard(); //clears board
+            //takes the player 1's inputs and creates a random generated number while there is not a winner
+            while (!game.checkForWin()) {
+                if (first == 1) {
+                    cout << "Your turn" << endl;
+                    cout << "Please enter row and column:" << endl;
+                    cin >> row1 >> col1;
+                    // check if the number player entered is reasonable
+                    while (row1 > 3 || col1 > 3 || row1 < 1 || col1 < 1) {
+                        cout << "Try again! The input can only be 1, 2, 3. " << endl;
+                        cin >> row1 >> col1;
+                    }
+                    while (!game.placeMark(row1 - 1, col1 - 1)) {
+                        cout << "This place was already filled." << endl;
+                        cin >> row1 >> col1;
+                    }
+                    cout << "You:" << endl;
+                    game.placeMark(row1 - 1, col1 - 1);
+                    game.printBoard();
+                    game.changePlayer();
+                    first++;
+                }
+                if (game.checkForWin()) {
+                    break;
+                }
+                if (game.isBoardFull()) {
+                    break;
+                }
+                //this will run when it is the computers turn and then will decrease firstMove at the end of the turn to allow player 1 a turn
+                if (first == 2) {
+                    //randomly generate rows and cols
+                    row1 = rand() % 3 + 1;
+                    col1 = rand() % 3 + 1;
+                    //if the place is filled, generate a new one
+                    while (!game.placeMark(row1 - 1, col1 - 1)) {
+                        row1 = rand() % 3 + 1;
+                        col1 = rand() % 3 + 1;
+                    }
+                    cout << "Computer:" << endl;
+                    game.placeMark(row1 - 1, col1 - 1);
+                    game.printBoard();
+                    game.changePlayer();
+                    first--;
+                    if (game.isBoardFull()) {
+                        break;
+                    }
+                    if (game.checkForWin()) {
+                        break;
+                    }
+                }
+            }
+            // print winner,
+            game.changePlayer();
+            if (game.getCurrentPlayerNumber() == 2) {
+                cout << "You win!" << endl;
+                game.printBoard();
+                cout << "Play again? (Type 'y'for yes and 'n' for no)" << endl;
+                cin >> playagain;
+            } else {
+                cout << "The computer win! " << endl;
+                game.printBoard();
+                cout << "Play again? (Type 'y'for yes and 'n' for no)" << endl;
+                cin >> playagain;
+            }
+            //prints that there was a tie and asks if the user wants to play again
+            if (game.isBoardFull()) {
+                cout << "Game draw! Play again? (Type 'y'for yes and 'n' for no)" << endl;
+                cin >> playagain;
             }
         }
-
-        else if (numPlayers == 2) {
+    } else if (numPlayers == 2) {
+        while (playagain != 'n') {
             cout << "The Game has Started! Player 1 goes first! Make sure to put a space between two numbers!" << endl;
             for (int i = 0; i < 9; i++) {
                 //Because there's a total nine spaces in the board, no more than 9 times will needed.
                 //check if the board full, if the board is full and no one has won, game draw
                 if (game.isBoardFull()) {
-                    cout << "Game Draw!";
-                    break;
+                    cout << "Game draw! Play again? (Type 'y'for yes and 'n' for no)" << endl;
+                    cin >> playagain;
                 } else {
                     int row1, col1;
                     cout << "Enter row and column:" << endl;
@@ -55,9 +122,10 @@ int main() {
                         // check if the number player entered is reasonable
                         if (row1 > 3 || col1 > 3 || row1 < 1 || col1 < 1) {
                             cout << "Try again! The input can only be 1, 2, 3. " << endl;
-                        }  else if (!game.placeMark(row1 - 1, col1 - 1)) {
+                        } else if (!game.placeMark(row1 - 1, col1 - 1)) {
                             cout << "This place was already filled." << endl;
-                        } else {
+                        }
+                        else {
                             break;
                         }
                     }
@@ -71,12 +139,11 @@ int main() {
                     }
                     //switch players
                     game.changePlayer();
-
                 }
             }
-
+            cout << "Play again? (Type 'y'for yes and 'n' for no)" << endl;
+            cin >> playagain;
         }
-
-
-
+    }
 }
+
